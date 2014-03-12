235ec1600f9054432cbd0e2e147d12926a93af4a
PikachuEXE <pikachuexe@gmail.com>
2014-03-12 15:10:36 +0800

    - Remove an invalid header

--- lib/action_controller_tweaks/caching.rb
+++ lib/action_controller_tweaks/caching.rb
@@ -6,8 +6,7 @@ module ActionControllerTweaks
 
     HEADERS = {
       "Cache-Control" => "no-cache, no-store, max-age=0, must-revalidate, pre-check=0, post-check=0", # HTTP 1.1
-      "no-cache" => "no-cache", # HTTP 1.0
-      "Expires" => "Fri, 01 Jan 1990 00:00:00 GMT", # Proxies
+      "Expires" => "Fri, 01 Jan 1990 00:00:00 GMT", # HTTP 1.0
     }.freeze
 
     included do
