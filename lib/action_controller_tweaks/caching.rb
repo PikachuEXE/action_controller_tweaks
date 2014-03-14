d41d7eeb98c4a29b42e49892b147072df6dc45e7
PikachuEXE <pikachuexe@gmail.com>
2014-03-14 16:10:04 +0800

    + Add back missing header for HTTP 1.0

--- lib/action_controller_tweaks/caching.rb
+++ lib/action_controller_tweaks/caching.rb
@@ -6,6 +6,7 @@ module ActionControllerTweaks
 
     HEADERS = {
       "Cache-Control" => "no-cache, no-store, max-age=0, must-revalidate, pre-check=0, post-check=0", # HTTP 1.1
+      "Pragma" => "no-cache", # HTTP 1.0
       "Expires" => "Fri, 01 Jan 1990 00:00:00 GMT", # HTTP 1.0
     }.freeze
 
